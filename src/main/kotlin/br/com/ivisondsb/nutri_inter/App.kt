bd2c9ec2c29eec9bd95810c91d83b02380906682
Ivisondsb <Ivisonk0@gmail.com>
2024-10-08 10:19:29 -0300

    first commit

---
+++ src/main/kotlin/br/com/ivisondsb/nutri_inter/App.kt
@@ -0,0 +1,11 @@
+package br.com.ivisondsb.nutri_inter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class App
+
+fun main(args: Array<String>) {
+	runApplication<App>(*args)
+}

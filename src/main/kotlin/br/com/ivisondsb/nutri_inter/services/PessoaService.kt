800b36d3ea09bc8dd492db9b6a8cd9cd02397bf3
ivisondsb <ivisonk0@gmail.com>
2024-10-16 16:13:49 -0300

    create PessoaService

---
+++ src/main/kotlin/br/com/ivisondsb/nutri_inter/services/PessoaService.kt
@@ -0,0 +1,12 @@
+package br.com.ivisondsb.nutri_inter.services
+
+import br.com.ivisondsb.nutri_inter.models.Pessoa
+import br.com.ivisondsb.nutri_inter.repositories.PessoaRepository
+
+class PessoaService(private val pessoaRepository: PessoaRepository) {
+
+    fun save(pessoa: Pessoa): Pessoa = pessoaRepository.save(pessoa)
+    fun findAll(): List<Pessoa> = pessoaRepository.findAll()
+    fun findById(id: Long): Pessoa? = pessoaRepository.findById(id).orElse(null)
+    fun deleteById(id: Long) = pessoaRepository.deleteById(id)
+}

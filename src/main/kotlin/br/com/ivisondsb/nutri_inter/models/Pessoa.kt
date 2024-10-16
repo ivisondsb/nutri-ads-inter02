970ead1c1aa75a3b20028f3d00c446a06b636be7
ivisondsb <ivisonk0@gmail.com>
2024-10-16 17:45:17 -0300

    map Pessoa table and columns and add the estado attribute

--- src/main/kotlin/br/com/ivisondsb/nutri_inter/models/Pessoa.kt
+++ src/main/kotlin/br/com/ivisondsb/nutri_inter/models/Pessoa.kt
@@ -1,27 +1,41 @@
 package br.com.ivisondsb.nutri_inter.models
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Id
+import jakarta.persistence.*
 import java.time.LocalDate
 
 @Entity
+@Table(name = "pessoas")
 data class Pessoa(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     val id: Long = 0,
+    @Column(name = "nome")
     val nome: String,
+    @Column(name = "sexo")
     val sexo: Char,
+    @Column(name = "cpf")
     val cpf: String,
+    @Column(name = "data_nascimento")
     val dataDeNascimento: LocalDate,
+    @Column(name = "idade")
     val idade: Int = LocalDate.now().year - dataDeNascimento.year,
+    @Column(name = "telefone")
     val telefone: String,
+    @Column(name = "email")
     val email: String,
+    @Column(name = "cep")
     val cep: String,
+    @Column(name = "endereco")
     val endereco: String,
+    @Column(name = "complemento")
     val complemento: String,
+    @Column(name = "numero_casa")
     val numeroDaCasa: Int,
+    @Column(name = "bairro")
     val bairro: String,
-    val cidade: String
+    @Column(name = "cidade")
+    val cidade: String,
+    @Column(name = "estado")
+    val estado: String
 )
